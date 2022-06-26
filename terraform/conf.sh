9a7b7453ad1031d00fa3ade4a0e1ee7840f983a2
Bryan Winkenbach <57653752+thetaxfraudguy@users.noreply.github.com>
2022-06-26 01:07:24 -0700

    feat:terraform

---
+++ terraform/conf.sh
@@ -0,0 +1,7 @@
+# shellcheck disable=SC2148
+alias tf="terraform"
+alias tfi="terraform init"
+alias tfv="terraform validate"
+alias tff="terraform format --recursive"
+alias tfa="terraform apply"
+alias tfaa="terraform apply -auto-approve"
