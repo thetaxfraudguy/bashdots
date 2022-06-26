1c84658055dbd873fc63e95341096143259cb329
Bryan Winkenbach <57653752+thetaxfraudguy@users.noreply.github.com>
2022-06-26 00:53:01 -0700

    fix:bash fixups

--- 00-brew/install.sh
+++ 00-brew/install.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 if [ "$(uname -s)" = "Darwin" ]; then
   if command -v brew >/dev/null 2>&1; then
-    echo "brew installed, updating"
-    # brew update
+    brew update
   else
-    echo "brew not installed. WTF?"
-    # /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   fi
   echo "$(dirname "$0")/Brewfile"
-#   brew bundle --file
+  brew bundle --file "$(dirname "$0")/Brewfile"
 fi
