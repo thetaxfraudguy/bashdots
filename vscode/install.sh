7b18bfad0b9d978b62c2d909c47f11681eff6ee5
Bryan Winkenbach <57653752+thetaxfraudguy@users.noreply.github.com>
2022-06-26 23:32:36 -0700

    feat:vscode

---
+++ vscode/install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+if command -v code >/dev/null; then
+  if [ "$(uname -s)" = "Darwin" ]; then
+    VSCODE_HOME="$HOME/Library/Application Support/Code"
+  else
+    VSCODE_HOME="$HOME/.config/Code"
+  fi
+  mkdir -p "$VSCODE_HOME/User"
+
+  ln -sf "$DOTFILES/vscode/settings.json" "$VSCODE_HOME/User/settings.json"
+
+  while read -r module; do
+    code --install-extension "$module" || true
+  done <"$DOTFILES/vscode/extensions.txt"
+fi
